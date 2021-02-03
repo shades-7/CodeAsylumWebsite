2d0820555f4402213358f18d149136aae352c52e
saloni <salonimark7@gmail.com>
2021-02-04 00:35:59 +0530

    landing done

---
+++ src/pages/Landing/landing.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styles from './landing.module.scss';
+import NavBar from '../../components/NavBar/navbar'
+
+import Art from '../../images/art.png';
+
+
+
+const App =() =>{
+    return(
+        <>
+         <div className={styles.landing}>
+            <NavBar />
+            <div className={styles.Container1}>
+                <div className={styles.Child1}>
+                    <div className={styles.heading}>
+                        <p>Start <span style={{color: "#2E368F"}}> coding </span> your future today.</p>
+                    </div>
+                    <div className={styles.subheading}>
+                        <p>Learn from the best</p>
+                    </div>
+                    <div className={styles.containt}>
+                        <p>Become a star developer in under 3 months <br></br> 
+                        Intense Bootcamp delivered by industry veterans <br></br>
+                        Coding Bootcamp for both Beginners and Experts</p>
+                    </div>
+                    <div className={styles.button}>
+                        <button className={styles.button1}>Online Bootcamp</button>
+                        <button className={styles.button2}>Offline Bootcamp</button>
+                    </div>
+                </div>
+                <div className={styles.Child2}>
+                    <img src={ Art } alt="Art Work" width='100%'/>
+                </div>
+            </div>
+         </div>
+        </>
+    )
+
+
+}
+
+
+export default App;
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
-import { Link } from "gatsby"
-import WebCourse from '../pages/Webcourse/Webcourse'
+// import { Link } from "gatsby"
+import Landing from '../pages/Landing/landing'
 
 
 import SEO from "../components/seo"
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <>
     <SEO title="Home" />
-    <WebCourse />
+    <Landing />
 
   </>
 )
