5143e19342006c5cafb3ab4547c0cf4d79a77f79
shades-7 <salonimark7@gmail.com>
2021-02-03 16:10:41 +0530

    layout made

---
+++ src/pages/Webcourse/Webcourse.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Webcourse.module.scss';
+
+const App =() =>{
+    return(
+        <>
+         <div className={styles.hello}>hello</div>
+        </>
+    )
+
+
+}
+
+export default App;
+
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+import WebCourse from '../pages/Webcourse/Webcourse'
+
+
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <>
+    <SEO title="Home" />
+    <WebCourse />
+
+  </>
+)
+
+export default IndexPage
