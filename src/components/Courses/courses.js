7caa7ba797d20840834a3dd2f4c410a9017b89c6
saloni <salonimark7@gmail.com>
2021-02-05 23:14:14 +0530

    sturctured course component

---
+++ src/components/Courses/courses.js
@@ -0,0 +1,13 @@
+import React from "react"
+import styles from "./courses.module.scss"
+import Layout from "../layout"
+
+const courses = () => {
+    return(
+        <Layout>
+            
+        </Layout>
+    )
+}
+
+export default courses;
