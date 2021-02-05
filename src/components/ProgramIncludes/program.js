56e918da32fa0468f3c09fbbc32cfd2ab1257745
saloni <salonimark7@gmail.com>
2021-02-05 17:45:02 +0530

    need optimization

--- src/components/ProgramIncludes/program.js
+++ src/components/ProgramIncludes/program.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from './program.module.scss'
 import Layout from '../layout'
 
+import Pic1 from '../../images/pic1.png'
+
 const program = () =>{
     return(
         <Layout>
@@ -10,8 +12,86 @@ const program = () =>{
                 <p>All our <span style={{color: "#2E368F"}}>programs</span> include</p>
                 </div>
                 <div className={styles.Child1}>
+                    <div className={styles.Program}>
+                        <div className={styles.Img}>
+                            <img src={ Pic1 } alt="Art Work" width='100%'/>
+                        </div>
+                        <div className={styles.Text}>
+                            <div className={styles.Subheading}>
+                                <p>Real-world projects from<br></br>
+                                <span style={{ borderBottom: "solid 5px #FFCC27"}}>industry experts</span></p>
+                            </div>
+                            <div className={styles.Content}>
+                                <p>
+                                With real world projects and immersive<br></br>
+                                content built in partnership with top<br></br>
+                                tier companies, you’ll master the tech<br></br>
+                                skills companies want.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.Program}>
+                        <div className={styles.Img}>
+                            <img src={ Pic1 } alt="Art Work" width='100%'/>
+                        </div>
+                        <div className={styles.Text}>
+                            <div className={styles.Subheading}>
+                                <p>Real-world projects from<br></br>
+                                industry experts</p>
+                            </div>
+                            <div className={styles.Content}>
+                                <p>
+                                With real world projects and immersive<br></br>
+                                content built in partnership with top<br></br>
+                                tier companies, you’ll master the tech<br></br>
+                                skills companies want.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className={styles.Child1}>
+                    <div className={styles.Program}>
+                        <div className={styles.Img}>
+                            <img src={ Pic1 } alt="Art Work" width='100%'/>
+                        </div>
+                        <div className={styles.Text}>
+                            <div className={styles.Subheading}>
+                                <p>Real-world projects from<br></br>
+                                <span style={{ borderBottom: "solid 5px #FFCC27"}}>industry experts</span></p>
+                            </div>
+                            <div className={styles.Content}>
+                                <p>
+                                With real world projects and immersive<br></br>
+                                content built in partnership with top<br></br>
+                                tier companies, you’ll master the tech<br></br>
+                                skills companies want.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.Program}>
+                        <div className={styles.Img}>
+                            <img src={ Pic1 } alt="Art Work" width='100%'/>
+                        </div>
+                        <div className={styles.Text}>
+                            <div className={styles.Subheading}>
+                                <p>Real-world projects from<br></br>
+                                industry experts</p>
+                            </div>
+                            <div className={styles.Content}>
+                                <p>
+                                With real world projects and immersive<br></br>
+                                content built in partnership with top<br></br>
+                                tier companies, you’ll master the tech<br></br>
+                                skills companies want.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </Layout>
