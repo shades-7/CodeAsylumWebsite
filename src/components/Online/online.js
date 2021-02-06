eb0b51a836cadb696ea4f6838138908825566b47
saloni <salonimark7@gmail.com>
2021-02-06 15:27:07 +0530

    online cards

---
+++ src/components/Online/online.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../Online/online.module.scss'
+import Layout from '../layout'
+
+const online = () => {
+    return(
+        <Layout>
+
+        </Layout>
+    )
+}
+
+export default online;
