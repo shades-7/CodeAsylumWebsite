d323116be0d4fa38174239ff8a214c8cc636fc71
saloni <salonimark7@gmail.com>
2021-02-06 17:05:10 +0530

    structured file

---
+++ src/components/PlacementAssistance/placement.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './placement.module.scss';
+import Layout from '../layout'
+
+const placement = () => {
+    return(
+        <Layout>
+            
+        </Layout>
+    )
+}
+
+export default placement;
