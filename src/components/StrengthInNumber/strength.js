193db37b1701426586d05640ee9a9ce4cb9e8a6d
saloni <salonimark7@gmail.com>
2021-02-08 22:43:51 +0530

    added header

--- src/components/StrengthInNumber/strength.js
+++ src/components/StrengthInNumber/strength.js
@@ -5,7 +5,14 @@ import Layout from '../layout';
 const strength = () =>{
     return(
         <Layout>
-            
+            <div className={styles.Container}>
+                <div className={styles.header}>
+                    <p>Strength in <span style={{color:'#2E388E'}}>numbers</span></p>
+                </div>
+                <div className={styles.Container1}>
+
+                </div>
+            </div>
         </Layout>
     )
 }
