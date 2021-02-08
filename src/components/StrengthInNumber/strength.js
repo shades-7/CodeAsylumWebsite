b9ef54e225b92c314a9541737e727bdf8254a29f
saloni <salonimark7@gmail.com>
2021-02-08 22:30:57 +0530

    added strength in number

---
+++ src/components/StrengthInNumber/strength.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './strength.module.scss';
+import Layout from '../layout';
+
+const strength = () =>{
+    return(
+        <Layout>
+            
+        </Layout>
+    )
+}
+
+export default strength;
