d8f664849ad5ff51048147d9cfe1549d9956fe5b
saloni <salonimark7@gmail.com>
2021-02-08 22:32:10 +0530

    home page updated

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -5,6 +5,7 @@ import NavBar from '../../components/NavBar/navbar';
 import Landing from '../../components/Landing/landing';
 import Program from '../../components/ProgramIncludes/program';
 import Courses from '../../components/Courses/courses';
+import Strength from '../../components/StrengthInNumber/strength';
 
 
 const App =() =>{
@@ -16,6 +17,7 @@ const App =() =>{
          </div>
          <Program />
          <Courses />
+         <Strength />
         </>
     )
 }
