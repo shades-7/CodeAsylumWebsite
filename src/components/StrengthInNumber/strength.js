79f85ed9323d9dee0ea1b29d3373ce9952eea87c
saloni <salonimark7@gmail.com>
2021-02-08 23:04:51 +0530

    pictures added

--- src/components/StrengthInNumber/strength.js
+++ src/components/StrengthInNumber/strength.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './strength.module.scss';
 import Layout from '../layout';
 
+import University from "../../images/university.png"
+
 const strength = () =>{
     return(
         <Layout>
@@ -10,6 +12,54 @@ const strength = () =>{
                     <p>Strength in <span style={{color:'#2E388E'}}>numbers</span></p>
                 </div>
                 <div className={styles.Container1}>
+
+                    <div className={styles.Container2}>
+                        <div className={styles.Image}>
+                            <img src={ University } alt=" " width="100%"/>
+                        </div>
+                        <div className={styles.number}>
+                            <p>200+</p>
+                        </div>
+                        <div className={styles.containt}>
+                            <p>Colleges Reached</p>
+                        </div>
+                    </div>
+
+                    <div className={styles.Container2}>
+                        <div className={styles.Image}>
+                            <img src={ University } alt=" " width="100%"/>
+                        </div>
+                        <div className={styles.number}>
+                            <p>200+</p>
+                        </div>
+                        <div className={styles.containt}>
+                            <p>Colleges Reached</p>
+                        </div>
+                    </div>
+
+                    <div className={styles.Container2}>
+                        <div className={styles.Image}>
+                            <img src={ University } alt=" " width="100%"/>
+                        </div>
+                        <div className={styles.number}>
+                            <p>200+</p>
+                        </div>
+                        <div className={styles.containt}>
+                            <p>Colleges Reached</p>
+                        </div>
+                    </div>
+
+                    <div className={styles.Container2}>
+                        <div className={styles.Image}>
+                            <img src={ University } alt=" " width="100%"/>
+                        </div>
+                        <div className={styles.number}>
+                            <p>200+</p>
+                        </div>
+                        <div className={styles.containt}>
+                            <p>Colleges Reached</p>
+                        </div>
+                    </div>
 
                 </div>
             </div>
