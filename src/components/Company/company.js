2d17235964798afa616479eb94c29ee9070caddd
saloni <salonimark7@gmail.com>
2021-02-10 22:17:46 +0530

    logo added

--- src/components/Company/company.js
+++ src/components/Company/company.js
@@ -3,8 +3,22 @@ import styles from "./company.module.scss"
 import Layout from "../layout"
 
 import Google from "../../images/google.png";
+import Microsoft from "../../images/microsoft.png";
+import Flipkart from "../../images/flipkart.png";
+import Paytm from "../../images/paytm.png";
+import Walmart from "../../images/walmartLabs.png";
+import Amazon from "../../images/amazon.png";
+import Arcesium from "../../images/arcesium.png";
+import Goldman from "../../images/goldmanSachs,png";
+import Oyo from "../../images/oyo.png";
+import Vmware from "../../images/vmware.png";
+import Adobe from "../../images/Adobe.png";
+import Intuit from "../../images/intuit.png";
 
 const company = () => {
+
+    let logo = [ Google, Microsoft, Flipkart, Paytm, Walmart, Amazon, Arcesium, Goldman, Oyo, Vmware, Adobe, Intuit]
+
     return(
         <Layout>
             <div className={ styles.Container}>
