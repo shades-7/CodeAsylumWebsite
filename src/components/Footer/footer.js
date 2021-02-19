7cfcbd54d950c22a4993ce10031d3a109565b0b7
saloni <salonimark7@gmail.com>
2021-02-19 15:20:41 +0530

    initialphase done

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -19,9 +19,15 @@ const Footer = () => {
                         <p>CodeAsylums 2020</p>
                     </div>
                         <div className={styles.icons}>
-                            <img src={ Facebook } alt="facebook logo" width="15%"/>
-                            <img src={ Instagram } alt="instagram logo" width="15%"/>
-                            <img src={ Medium } alt="medium logo" width="15%"/>
+                            <a href="https://www.facebook.com/CodeAsylums/">
+                                <img src={ Facebook } alt="facebook logo" width="55%"/>
+                            </a>
+                            <a href="https://www.instagram.com/codeasylums/?hl=en">
+                                <img src={ Instagram } alt="instagram logo" width="55%"/>
+                            </a>
+                            <a href="https://medium.com/progate/kickstarting-careers-during-summer-holidays-with-a-4-week-programming-bootcamp-f807da834d83">
+                                <img src={ Medium } alt="medium logo" width="55%"/>
+                            </a>
                         </div>
                 </div>
                 <div className= {styles.Child2}>
@@ -29,16 +35,16 @@ const Footer = () => {
                         <p>Bootcamps</p>
                     </div>
                     <div className={styles.course}>
-                        <p>Full Stack Web Development</p>
+                    <a href="https://ace.codeasylums.com/fullstack"><p>Full Stack Web Development</p></a>
                     </div>
                     <div className={styles.course}>
-                        <p>Data Structures & Algorithms</p>
+                        <a href="https://ace.codeasylums.com/dsa"><p>Data Structures & Algorithms</p></a>
                     </div>
                     <div className={styles.course}>
-                        <p>Data Analytics</p>
+                    <a href="https://ace.codeasylums.com/danalytics"><p>Data Analytics</p></a>
                     </div>
                     <div className={styles.course}>
-                        <p>Data Science with Machine Learning</p>
+                    <a href="https://ace.codeasylums.com/datascience"><p>Data Science with Machine Learning</p></a>
                     </div>
                 </div>
                 <div className= {styles.Child2}>
