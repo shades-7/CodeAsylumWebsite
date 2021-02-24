ffa15181d0cfc8f475b459f8a2cd53240ad1b297
saloni <salonimark7@gmail.com>
2021-02-24 16:38:59 +0530

    updating grads

--- src/components/Graduates/graduates.js
+++ src/components/Graduates/graduates.js
@@ -3,6 +3,8 @@ import styles from './graduates.module.scss';
 import Layout from '../layout';
 
 import grad1 from '../../images/grad1.png';
+import grad2 from '../../images/grad2.png';
+import grad3 from '../../images/grad3.png';
 import linkdien from '../../images/linkdien.png'
 
 const graduates = () =>{
@@ -15,16 +17,16 @@ const graduates = () =>{
             "account" : "shivam's linkdien account"
         },
         {
-            "image" : grad1,
-            "name" : "Shivam Raj",
+            "image" : grad2,
+            "name" : "Shivansh ",
             "containt" : "CodeAsylums has played a valuable role in helping me land a package of 51L at Atlassian. I'm grateful for the constant support and the invaluable resources that they provided. With my never give up attitude and the need to constantly learn and grow, I realised CodeAsylums was just the assistance I needed.",
-            "account" : "shivam's linkdien account"
+            "account" : "shivansh's linkdien account"
         },
         {
-            "image" : grad1,
-            "name" : "Shivam Raj",
+            "image" : grad3,
+            "name" : "Aman",
             "containt" : "CodeAsylums has played a valuable role in helping me land a package of 51L at Atlassian. I'm grateful for the constant support and the invaluable resources that they provided. With my never give up attitude and the need to constantly learn and grow, I realised CodeAsylums was just the assistance I needed.",
-            "account" : "shivam's linkdien account"
+            "account" : "aman's linkdien account"
         }
     ]
 
