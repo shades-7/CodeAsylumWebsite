32307fccad6a7c05a2732ca4037f5daa1ce81f45
saloni <salonimark7@gmail.com>
2021-02-24 14:31:13 +0530

    updating grads

---
+++ src/components/Graduates/graduates.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './graduates.module.scss';
+import Layout from '../layout';
+
+const graduates = () =>{
+    return(
+        <Layout>
+            <p>hello</p>
+        </Layout>
+    )
+}
+export default graduates;
