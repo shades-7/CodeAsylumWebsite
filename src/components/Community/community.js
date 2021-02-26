e48cdb2caa1341fba702cbd1eb4c3dff81f189fb
saloni <salonimark7@gmail.com>
2021-02-26 15:23:58 +0530

    community updated

---
+++ src/components/Community/community.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from 'community.module.scss';
+import Layout from '../layout';
+
+const community = () => {
+    return(
+        <Layout>
+        </Layout>
+    )
+}
+
+export default community;
