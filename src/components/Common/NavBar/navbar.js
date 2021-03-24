f1dc50a076a9c9343ad44a7a2cd4de481b12905d
saloni <salonimark7@gmail.com>
2021-03-24 12:00:11 +0530

    dropdown menu styled

--- src/components/Common/NavBar/navbar.js
+++ src/components/Common/NavBar/navbar.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './navbar.module.scss';
 import Layout from '../layout'
 import Logo from '../../../images/LogoBlack.png';
 import LogoWhite from '../../../images/LogoWhite.png';
-import Menu from '../../../images/menu1.png';
+import Menu1 from '../../../images/menu1.png';
 import { Link } from "gatsby"
 import * as Scroll from "react-scroll"
 
+import icon1 from '../../../images/scholar.png'
+import icon2 from '../../../images/icon2.png'
+import icon3 from '../../../images/icon3.png'
+
+
+
 let SmoothLink = Scroll.Link;
 
-const navbar = () =>{
+
+const Navbar = (props) =>{
+
+    const [open, setOpen] = useState(false);
+
+    const Content = [
+      {
+        "link" : `/Mentor/mentor`,
+        "image" : icon1,
+        "text" : "Bootcamp"
+      },
+      {
+        "link" : `/Mentor/mentor`,
+        "image" : icon2,
+        "text" : "Mentor"
+      },
+      {
+        "link" : `/Mentor/mentor`,
+        "image" : icon3,
+        "text" : "Alumni"
+      }
+    ]
+
     return(
         <Layout>
             <div className={styles.navbar}>
@@ -19,12 +47,34 @@ const navbar = () =>{
                 <Link to={`/`} className={styles.link}>Alumni</Link>
                 <Link to={`/Mentor/mentor`} className={styles.link}>Mentors</Link>
                 <div className={styles.space}></div>
-                <button className={styles.button2}>Request Callback</button>
-                <img className={styles.logoWhite} src={ LogoWhite } alt="Logo"/> 
-                <img className={styles.menu} src={ Menu } alt="Hamburger Menu" />
+                <button className={styles.button2}>Request Callback</button>  
             </div>
+
+            <div className={styles.navbar}>
+                <img className={styles.logoWhite} src={ LogoWhite } alt="Logo"/> 
+                
+                <img className={styles.menu} src={ Menu1 } onClick={() => {setOpen(!open)}} alt="Hamburger Menu" />
+
+                {open && <div className={styles.dropdown}>
+                  {
+                    Content.map((item,index) => {
+                      return(
+                        <Link to={ item.link } key={index}> 
+                          <div className={styles.item}>
+                              <div className={styles.icon}>
+                                <img className={styles.image} src = { item.image }/>
+                              </div>
+                              <div className={styles.text}>{ item.text }</div>
+                          </div>
+                        </Link>
+                      )
+                    })
+                  }   
+                </div>}
+            </div>
+                
         </Layout>
     )
 }
 
-export default navbar;
+export default Navbar;
